65c3b07f42f0a6bc638874e84f3c45dadcf3e96d
Matthiasfiebig94 <59293876+Matthiasfiebig94@users.noreply.github.com>
2020-01-10 23:26:59 +0100

    Basic GET Request -> DB wert auslesen

--- app/src/main/java/com/fiebig/blutzuckerapp/MainActivity.java
+++ app/src/main/java/com/fiebig/blutzuckerapp/MainActivity.java
@@ -1,14 +1,52 @@
 package com.fiebig.blutzuckerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    String server_url = "http://192.168.178.59/blutzuckerapp_api/blutzucker.php?user=1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.DBButton);
+        textView = findViewById(R.id.DBText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, server_url,
+
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                textView.setText(response);
+                                requestQueue.stop();
+                            }
+                        }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error){
+                            textView.setText(error.toString());
+                            requestQueue.stop();
+                        }
+                    });
+                requestQueue.add(stringRequest);
+            }
+        });
     }
 }
