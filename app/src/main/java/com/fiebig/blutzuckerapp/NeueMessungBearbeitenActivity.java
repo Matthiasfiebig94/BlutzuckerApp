a27c9d98c89b4db7a531dcdd0a7e3e8f08132d95
Matthiasfiebig94 <59293876+Matthiasfiebig94@users.noreply.github.com>
2020-03-02 22:45:58 +0100

    Dropdown angefangen

--- app/src/main/java/com/fiebig/blutzuckerapp/NeueMessungBearbeitenActivity.java
+++ app/src/main/java/com/fiebig/blutzuckerapp/NeueMessungBearbeitenActivity.java
@@ -1,15 +1,32 @@
 package com.fiebig.blutzuckerapp;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class NeueMessungBearbeitenActivity extends AppCompatActivity {
     String server_url = "http://192.168.178.59/blutzuckerapp_api/blutzucker.php?user=1";
-    ImageView neueMessungBearbeitenAbbrechen, neueMessungBearbeitenBestaetigen;
+    ImageView neueMessungBearbeitenAbbrechen, neueMessungBearbeitenBestaetigen, lebensmittelPopupClose;
+    EditText neueMessungMesswert;
+    Dialog gegessenPopup;
+    Button neueMessungGegessen;
+    Spinner lebensmittelSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +35,73 @@ public class NeueMessungBearbeitenActivity extends AppCompatActivity {
 
         neueMessungBearbeitenAbbrechen = findViewById(R.id.neueMessungBearbeitenAbbrechen);
         neueMessungBearbeitenBestaetigen = findViewById(R.id.neueMessungBearbeitenBestaetigen);
+        neueMessungMesswert = findViewById(R.id.neueMessungMesswert);
+        neueMessungGegessen = findViewById(R.id.neueMessungGegessen);
+        gegessenPopup = new Dialog(this, R.style.Popup);
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(NeueMessungBearbeitenActivity.this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, server_url,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        neueMessungMesswert.setText(response + " mg/dl");
+                        requestQueue.stop();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error){
+                neueMessungMesswert.setText(error.toString());
+                requestQueue.stop();
+            }
+        });
+        requestQueue.add(stringRequest);
+
+        neueMessungGegessen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gegessenPopup.setContentView(R.layout.neue_messung_lebensmittel_popup);
+                gegessenPopup.show();
+                lebensmittelPopupClose = (ImageView) gegessenPopup.findViewById(R.id.lebensmittelPopupClose);
+                lebensmittelSpinner = gegessenPopup.findViewById(R.id.lebensmittelSpinner);
+
+                String[] items = new String[]{"Banane", "Brot", "Traubenzucker"};
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+
+                lebensmittelPopupClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        gegessenPopup.dismiss();
+                    }
+                });
+
+
+                lebensmittelPopupClose.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Thread neueMessungThread = new Thread() {
+
+                            @Override
+                            public void run() {
+                                try {
+                                    gegessenPopup.dismiss();
+                                    super.run();
+                                } catch (Exception e) {
+
+                                } finally {
+                                }
+                            }
+                        };
+                        neueMessungThread.start();
+                    }
+                });
+            }
+        });
+
+
+
+
 
         neueMessungBearbeitenAbbrechen.setOnClickListener(new View.OnClickListener() {
             @Override
